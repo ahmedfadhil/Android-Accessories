dfe09d568ce7fc66936fd11f32fd30ca4ac4f0d8
ahmed fadhil <fadhil@fbk.eu>
2018-02-11 11:47:07 +0100

    added the notification manager

--- app/src/main/java/com/example/android/addmobey/MainActivity.java
+++ app/src/main/java/com/example/android/addmobey/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.android.addmobey;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.ads.AdRequest;
@@ -16,5 +21,17 @@ public class MainActivity extends AppCompatActivity {
         AdView adView = (AdView) findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         adView.loadAd(adRequest);
+
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 1, intent, 0);
+        Notification notification = new NotificationCompat.Builder(getApplicationContext())
+                .setContentTitle("Lunch is ready")
+                .setContentText("Hurry up")
+                .setContentIntent(pendingIntent)
+                .addAction(android.R.drawable.sym_def_app_icon, "Chat", pendingIntent)
+                .setSmallIcon(android.R.drawable.sym_def_app_icon)
+                .build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
     }
 }
